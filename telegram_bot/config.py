974ce0fb46630d57b907e24e076b5e4ac5e976ac
VadimShabashov <57601499+VadimShabashov@users.noreply.github.com>
2021-12-02 02:35:52 +0300

    Delete config.py

--- telegram_bot/config.py
+++
@@ -1,9 +0,0 @@
-# Telegram bot
-API_KEY = '2104213549:AAGprzSYiO2zAXCUTIkvZugc1aRQK2UVTi8'
-
-# Database
-HOST_NAME = "localhost"
-DATABASE_NAME = "interviewassistant"
-USER_NAME = "postgres"
-PASSWORD = "postgres"
-PORT = 5432
